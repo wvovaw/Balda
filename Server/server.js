5ada97f856f8c8a5342e2b3def0a85ccd7a2c6dc
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2020-04-18 13:22:58 +0500

    The main server logic implement

--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,50 @@
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
+let connectedUsers = [];
+let lobbyList = [];
 
-const app = express();
-const clientPath = `${__dirname}/../main.js;`;
-console.log(`Serving static from ${clientPath}`);
-app.use(express.static(clientPath));
-
-const server = http.createServer(app);
-
-const io = socketio(server);
-
-io.on('connect', (sock) => {
-    sock.emit('message', 'You are connected');
-} )
-
-server.on('error', (e) => {
-    console.log('Server error:', e);
-});
-
-server.listen(8080, () => {
-    console.log('The Balda started on 8080');
-}); 
+class Lobby {
+    constructor(lobbyName, hostUser) {
+        this.lobbyName = lobbyName;
+        this.addPlayer(hostUser);
+        lobbyList.push(this);
+        console.log(`Lobby '${this.lobbyName}' was succsessfully created by ${this.hostUser}`);
+    }
+    playerList = Array(); //userName
+    addPlayer(userName) {
+        if(this.playerList.length < 5) {
+            player = new Player(userName);
+            this.playerList.push(player);
+            console.log(`Player ${this.player.userName} has succsessfuly connected to lobby ${this.lobbyName}`);
+        } 
+    }
+    kickPlayer(userName) {
+        var pos = this.playerList.indexOf(userName);
+        if(pos > -1) {
+            var removedPlayer = playerList[pos].userName;
+            this.playerList.splice(pos, 1);
+            console.log(`Player ${removedPlayer} has disconnected from the '${this.lobbyName}'`);
+        } 
+    }
+    startGame() {
+       //Send start game signal to all clients from the playerList
+       //there is gonna be an implementation of the game cycle (player 1, 2, 3, 4, 5) untill the gamefield isn't filled
+       
+    }
+    endGame() {
+        //End game signal to trigger end-game window 
+    }
+}
+class User {
+    constructor(userName, userAvatar) {
+      this.userName = userName;
+      this.userAvatar = userAvatar;
+    }
+}
+class Player {
+    constructor(userName) {
+        this.playerName = userName;
+    }
+    #points = 0;
+    addPoints(points) {
+        this.#points += points;
+    }
+}
