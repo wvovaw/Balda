d9302acd30c27ccf8b634b256dd2ed61110252f0
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2020-04-19 08:48:16 +0500

    Add dark-mini.css micro-lib, index.html moved to game.html

--- renderer.js
+++ renderer.js
@@ -1,9 +1,9 @@
 const {remote, ipcRenderer} = require('electron')
 
 document.getElementById('minimize_button').addEventListener('clicl', () => {
-  remote.getCurrentWindow().minimize();
-})
+  remote.BrowserWindow.getFocusedWindow().minimize();
+});
 
 document.getElementById('close_button').addEventListener('click', () => {
     remote.app.quit();
-})
+});
