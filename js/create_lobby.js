fe8ec7d8b41a8ddbbab35b5b25ddeaaa54a97ecf
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2020-04-20 19:43:52 +0500

    Add functions to create lobby window

--- js/create_lobby.js
+++ js/create_lobby.js
@@ -1,5 +1,24 @@
 let dia = remote.getCurrentWindow();
 document.getElementById('cancel_button').addEventListener('click', () => {
-    console.log('close!');
     dia.close();
+});
+let ic = document.getElementById('i_lock')
+ic.addEventListener('click', () => {
+  if(ic.className == 'fas fa-unlock') {
+    ic.className = 'fas fa-lock';
+    document.getElementById('pass').className += 'innactive';
+    document.getElementById('pass').value = '';
+  }
+  else {
+    ic.className = 'fas fa-unlock';
+    document.getElementById('pass').className = '';
+  }
+});
+document.getElementById('accept_button').addEventListener('click', () => {
+    document.getElementById('lobby_title');
+    document.getElementById('number_of_players');
+    document.getElementById('lobby_title');
+    //Сохраняем инпут, отпаравляем серверу и закрываем окно
+    dia.close();
+
 });
